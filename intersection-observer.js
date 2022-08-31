4ca09c08dc1ba0ab126179f3082e66c3c6785fd6
Cristina <you@example.com>
2022-08-31 07:31:08 +0300

    js intersection observer changes

--- intersection-observer.js
+++ intersection-observer.js
@@ -1,21 +1,22 @@
-const sectionOne = document.querySelector('.section1');
-const sections = document.querySelectorAll('section')
+const sections = document.querySelectorAll('section');
+
 const options = {
-    root: null, //it is the viewport
-    treshold: 0.25,
-    rootMargin: '-150px' //margin on viewport - distance on when to fire the comand
-};
-const observer = new IntersectionObserver(function(entries, observer) {
+    root: null,
+    threshold: 0,
+    rootMargin: '-50px'
+}
+const observer = new IntersectionObserver(function (entries, observer) {
     entries.forEach(entry => {
         if (!entry.isIntersecting) {
-            return
-        }
-        entry.target.classList.toggle('.is-visible');
-        observer.unobserve(entry.target);
-    })
-}, options);
+            entry.target.classList.remove('is-visible');
+            return;
+        } else {
+            entry.target.classList.add('is-visible');
 
-observer.observe(sectionOne);
+        }
+        
+    });
+}, options)
 
 sections.forEach(section => {
     observer.observe(section);
