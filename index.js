0e72b49a6f1cfdfe44edddc90dded40894d884f1
Cristina <you@example.com>
2022-09-24 19:57:17 +0300

    changes to chest sections

--- index.js
+++ index.js
@@ -51,28 +51,27 @@ const imgArr = document.getElementsByClassName("vending-machine")
 const singleService = document.getElementsByClassName(".single-service");
 
 function change() {
-    if (imgArr[0].classList.contains('active-img')) {
-      imgArr[0].classList.remove('active-img');
-      imgArr[1].classList.add('active-img');
-  } 
-   else if (arr[1].classList.contains('active-img')) {
-    imgArr[1].classList.remove('active-img');
-    imgArr[2].classList.add('active-img');
-   }
-   else {
-    imgArr[2].classList.remove('active-img');
-    imgArr[0].classList.add('active-img');
-   }
-};
-
+  if (imgArr[0].classList.contains('active-img')) {
+    imgArr[0].classList.remove('active-img');
+    imgArr[1].classList.add('active-img');
+} 
+ else if (imgArr[1].classList.contains('active-img')) {
+     imgArr[1].classList.remove('active-img');
+     imgArr[2].classList.add('active-img');
+ }
+ else {
+     imgArr[2].classList.remove('active-img');
+     imgArr[0].classList.add('active-img');
+ }
+}
 // Add function on click to change the class of the images so we can see a different picture with each click
 previous.addEventListener('click', () => {
-  change();
-});
+change()
+})
 
-let timer2 = setInterval(change, 1000);
+let timer2 = setInterval(change, 4000);
+
 function complete2() {
   clearInterval(timer2);
-  timer = null;
-};
-
+  timer2 = null;
+}
