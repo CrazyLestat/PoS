a172a8d0ec69ed503135979aae836f407abf2633
Cristina <you@example.com>
2022-09-24 16:50:45 +0300

    all changes

--- index.js
+++ index.js
@@ -43,3 +43,36 @@ function complete() {
   clearInterval(timer);
   timer = null;
 }
+
+/*CAROUSEL*/
+const next = document.querySelector('.next');
+const previous = document.getElementById('button-previous');
+const imgArr = document.getElementsByClassName("vending-machine")
+const singleService = document.getElementsByClassName(".single-service");
+
+function change() {
+    if (imgArr[0].classList.contains('active-img')) {
+      imgArr[0].classList.remove('active-img');
+      imgArr[1].classList.add('active-img');
+  } 
+   else if (arr[1].classList.contains('active-img')) {
+    imgArr[1].classList.remove('active-img');
+    imgArr[2].classList.add('active-img');
+   }
+   else {
+    imgArr[2].classList.remove('active-img');
+    imgArr[0].classList.add('active-img');
+   }
+};
+
+// Add function on click to change the class of the images so we can see a different picture with each click
+previous.addEventListener('click', () => {
+  change();
+});
+
+let timer2 = setInterval(change, 1000);
+function complete2() {
+  clearInterval(timer2);
+  timer = null;
+};
+
